67dca28a85d7eece62e7665a2dec00d36328a53d
Stevehunn <nicolas.bitaillou@etu.univ-tours.fr>
2022-08-02 22:09:13 +0200

    Ajout bouton

--- index.py
+++ index.py
@@ -1,9 +1,16 @@
+from cgitb import text
 import tkinter as tk
 
 app = tk.Tk()
 app.title("Video Downloader")
 canvas = tk.Canvas (app, width=400, height=200, background="purple")
 canvas.pack(fill="both",expand=True)
+
+message = tk.Label(app, text="Do you want to send the video in this directory ?")
+messageUrl =message
+messageUrlResponse =messageUrl
+answer_yes = message
+answer_no =answer_yes
 
 def action_bouton_yes():
     #phase 1
@@ -26,11 +33,38 @@ def action_bouton_no():
     answer_yes.destroy()
     answer_no.destroy()
 
+    return
 
+def phase0hide():
+    message.destroy()
+    messageUrl.destroy()
+    messageUrlResponse.destroy()
+    answer_yes.destroy()
+    answer_no.destroy()
+    
+    return
 
+def phase1hide():
+    buttonWelcome.destroy()
+    welcomeButton.destroy()
+    messageUrl.destroy()
+    messageUrlResponse.destroy()
     return
 
 
+
+phase0hide()
+#Init
+welcomeButton = tk.Label(app, text="Welcome to this video downlaoder app")
+welcomeButton.pack()
+welcomeButton.place(x=50, y= 50)
+
+buttonWelcome = tk.Button(app , text= "Yes", command=phase1hide)
+buttonWelcome.pack()
+buttonWelcome.place(x= 100, y=100)
+
+
+#Phase 1
 message = tk.Label(app, text="Do you want to send the video in this directory ?")
 message.pack()
 message.place(x=50, y= 50)
@@ -43,7 +77,19 @@ answer_no = tk.Button(app , text= "No", command=action_bouton_no)
 answer_no.pack()
 answer_no.place(x= 250, y=100)
 
+#Phase 2
+messageUrl = tk.Label(app, text="Enter the Youtube-url")
+messageUrl.pack()
+messageUrl.place(x=50, y= 50)
 
+messageUrlResponse = tk.Entry(app, text="truc")
+messageUrlResponse.place(x=50, y= 100)
+
+
+
+
+
+#Quit button
 close_app = tk.Button(app , text= "Quit", command= app.quit)
 close_app.pack()
 close_app.place(x= 360, y=170)
